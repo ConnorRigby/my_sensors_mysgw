98a4b3367101ab1e3e4c3de285c99d6c5ac16581
connor rigby <connor@farmbot.io>
2017-12-29 13:53:37 -0800

    Make gw cleanup after itself

--- lib/my_sensors.mysgw.ex
+++ lib/my_sensors.mysgw.ex
@@ -13,6 +13,10 @@ defmodule MySensors.MySGW do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
   end
 
+  def stop(reason \\ :normal) do
+    GenServer.stop(__MODULE__, reason)
+  end
+
   def init([]) do
     exe = Path.join([:code.priv_dir(:my_sensors_mysgw), "my_sensors/mysgw"])
     port_opts = [
@@ -42,10 +46,19 @@ defmodule MySensors.MySGW do
   end
 
   def handle_info(info, state) do
-    {:stop, {:unexpected_info, info}}
+    {:stop, {:unexpected_info, info}, state}
   end
 
-  def terminate(reason, _state) do
-    Logger.error "mysgw died: #{inspect reason}"
+  def terminate(reason, state) do
+    unless reason in [:normal, :shutdown] do
+      Logger.error "mysgw died: #{inspect reason}"
+    end
+    if state.port do
+      info = Port.info(state.port)
+      os_pid = Keyword.get(info, :os_pid)
+      if os_pid do
+        System.cmd("kill", ["15", "#{os_pid}"], into: IO.stream(:stdio, :line))
+      end
+    end
   end
 end
